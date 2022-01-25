1bdf74cad791908049f43abf0b743d1dc02c2aee
duynn9433 <92941533+duynn9433@users.noreply.github.com>
2022-01-25 14:37:05 +0700

    Initial commit

---
+++ app/src/main/java/com/duynn/memorableplaces/MainActivity.java
@@ -0,0 +1,41 @@
+package com.duynn.memorableplaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Add Location");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,stringList);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                intent.putExtra("placeNumber", position);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
